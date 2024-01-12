7f439bd4453e5b9264418173724ff4df148af1f1
Bhavika <bhavikamr17@gmail.com>
2024-01-12 05:37:38 +0530

    error fixed Db to DB

--- src/components/blogPostcard/BlogPostCard.jsx
+++ src/components/blogPostcard/BlogPostCard.jsx
@@ -1,67 +1,180 @@
-import React from 'react'
-import { useContext } from 'react'
-import { Button } from '@material-tailwind/react';
-import myContext from '../../context/data/myContext';
+// import React from 'react'
+// import { useContext } from 'react'
+// import { Button } from '@material-tailwind/react';
+// import myContext from '../../context/data/myContext';
 
+
+// function BlogPostCard() {
+//   const context= useContext(myContext);
+//   const {mode} = context
+
+//   return (
+//     <div>
+//    <section className='text-gray-500 body-font'>
+//    <div className='container px-5 py-10 mx-auto max-w-7xl'>
+//     <div className='flex flex-wrap justify-center -m-4 mb-5'>
+//       {/* card 1*/}
+//       <div className='p-4 md:w-1/3'>
+//         <div className=''>
+//          <img className='w-full' src={'src/assets/tree-736885_1280 (1).jpg'}/>
+//          <div className='p-6'>
+//          <h2 className='tracking-widset text-x5 title-font font-medium text-gray-400 mb-1'>
+//           {'11 jan 2024'}
+//          </h2>
+//           <h1 className='title-font text-lg font-bold text-gray-900 mb-3'>{'Culture of india'}</h1>
+//           <p>india is veru rich culturalral heritage etc.</p>
+//          </div>
+//         </div> 
+//       </div>
+
+//   {/* card 2*/}
+//   <div className='p-4 md:w-1/3'>
+//         <div className=''>
+//          <img className='w-full' src={'src/assets/tree-736885_1280 (1).jpg'}/>
+//          <div className='p-6'>
+//          <h2 className='tracking-widset text-x5 title-font font-medium text-gray-400 mb-1'>
+//           {'11 jan 2024'}
+//          </h2>
+//           <h1 className='title-font text-lg font-bold text-gray-900 mb-3'>{'Culture of india'}</h1>
+//           <p>india is veru rich culturalral heritage etc.</p>
+//          </div>
+//         </div> 
+//       </div>
+
+//   {/* card 3*/}
+//   <div className='p-4 md:w-1/3'>
+//         <div className=''>
+//          <img className='w-full' src={'src/assets/tree-736885_1280 (1).jpg'}/>
+//          <div className='p-6'>
+//          <h2 className='tracking-widset text-x5 title-font font-medium text-gray-400 mb-1'>
+//           {'11 jan 2024'}
+//          </h2>
+//           <h1 className='title-font text-lg font-bold text-gray-900 mb-3'>{'Culture of india'}</h1>
+//           <p>india is veru rich culturalral heritage etc.</p>
+//          </div>
+//         </div> 
+//       </div>
+
+//     </div>
+//    <div className='flex justify-center my-5'>
+//     <Button>See More</Button>
+//    </div>
+//    </div>
+//    </section>
+//    </div>
+//   )
+// }
+
+// export default BlogPostCard
+
+import { Button } from '@material-tailwind/react'
+import React, { useContext } from 'react'
+import myContext from '../../context/data/myContext';
+import { useNavigate } from 'react-router';
 
 function BlogPostCard() {
-  const context= useContext(myContext);
-  const {mode} = context
+  const context = useContext(myContext);
+  const { mode, getAllBlog } = context;
+
+  const navigate = useNavigate();
 
   return (
     <div>
-   <section className='text-gray-500 body-font'>
-   <div className='container px-5 py-10 mx-auto max-w-7xl'>
-    <div className='flex flex-wrap justify-center -m-4 mb-5'>
-      {/* card 1*/}
-      <div className='p-4 md:w-1/3'>
-        <div className=''>
-         <img className='w-full' src={'src/assets/tree-736885_1280 (1).jpg'}/>
-         <div className='p-6'>
-         <h2 className='tracking-widset text-x5 title-font font-medium text-gray-400 mb-1'>
-          {'11 jan 2024'}
-         </h2>
-          <h1 className='title-font text-lg font-bold text-gray-900 mb-3'>{'Culture of india'}</h1>
-          <p>india is veru rich culturalral heritage etc.</p>
-         </div>
-        </div> 
-      </div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-10 mx-auto max-w-7xl ">
 
-  {/* card 2*/}
-  <div className='p-4 md:w-1/3'>
-        <div className=''>
-         <img className='w-full' src={'src/assets/tree-736885_1280 (1).jpg'}/>
-         <div className='p-6'>
-         <h2 className='tracking-widset text-x5 title-font font-medium text-gray-400 mb-1'>
-          {'11 jan 2024'}
-         </h2>
-          <h1 className='title-font text-lg font-bold text-gray-900 mb-3'>{'Culture of india'}</h1>
-          <p>india is veru rich culturalral heritage etc.</p>
-         </div>
-        </div> 
-      </div>
+          {/* Main Content  */}
+          <div className="flex flex-wrap justify-center -m-4 mb-5">
+            {/* Card 1  */}
+            {getAllBlog.length > 0
+              ?
+              <>
+                {getAllBlog.map((item, index) => {
+                  const { thumbnail, id, date } = item
+                  console.log(item)
+                  return (
+                    <div className="p-4 md:w-1/3" key={index}>
+                      <div
+                        style={{
+                          background: mode === 'dark'
+                            ? 'rgb(30, 41, 59)'
+                            : 'white',
+                          borderBottom: mode === 'dark'
+                            ?
+                            ' 4px solid rgb(226, 232, 240)'
+                            : ' 4px solid rgb(30, 41, 59)'
+                        }}
+                        className={`h-full shadow-lg  hover:-translate-y-1 cursor-pointer hover:shadow-gray-400
+                        ${mode === 'dark'
+                            ? 'shadow-gray-700'
+                            : 'shadow-xl'
+                            } 
+                        rounded-xl overflow-hidden`} 
+                      >
+                        {/* Blog Thumbnail  */}
+                        <img onClick={() => navigate(`/bloginfo/${id}`)} className=" w-full" src={thumbnail} alt="blog" />
 
-  {/* card 3*/}
-  <div className='p-4 md:w-1/3'>
-        <div className=''>
-         <img className='w-full' src={'src/assets/tree-736885_1280 (1).jpg'}/>
-         <div className='p-6'>
-         <h2 className='tracking-widset text-x5 title-font font-medium text-gray-400 mb-1'>
-          {'11 jan 2024'}
-         </h2>
-          <h1 className='title-font text-lg font-bold text-gray-900 mb-3'>{'Culture of india'}</h1>
-          <p>india is veru rich culturalral heritage etc.</p>
-         </div>
-        </div> 
-      </div>
+                        {/* Top Items  */}
+                        <div className="p-6">
+                          {/* Blog Date  */}
+                          <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1" style={{
+                            color: mode === 'dark'
+                              ? 'rgb(226, 232, 240)'
+                              : ' rgb(30, 41, 59)'
+                          }}>
+                            {date}
+                          </h2>
 
-    </div>
-   <div className='flex justify-center my-5'>
-    <Button>See More</Button>
-   </div>
-   </div>
-   </section>
-   </div>
+                          {/* Blog Title  */}
+                          <h1 className="title-font text-lg font-bold text-gray-900 mb-3" style={{
+                            color: mode === 'dark'
+                              ? 'rgb(226, 232, 240)'
+                              : ' rgb(30, 41, 59)'
+                          }}>
+                            {item.blogs.title}
+                          </h1>
+
+                          {/* Blog Description  */}
+                          <p className="leading-relaxed mb-3" style={{
+                            color: mode === 'dark'
+                              ? 'rgb(226, 232, 240)'
+                              : ' rgb(30, 41, 59)'
+                          }}>
+                            Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })}
+              </>
+              :
+              <>
+                <h1 className='text-xl font-bold'>Not Found</h1>
+              </>
+            }
+          </div>
+
+          {/* See More Button  */}
+          <div className="flex justify-center my-5">
+          <Link to={'/allblogs'}>
+            <Button
+              style={{
+                background: mode === 'dark'
+                  ? 'rgb(226, 232, 240)'
+                  : 'rgb(30, 41, 59)',
+                color: mode === 'dark'
+                  ?
+                  'rgb(30, 41, 59)'
+                  : 'rgb(226, 232, 240)'
+              }}>
+              See More
+            </Button>
+            </Link>
+          </div>
+        </div>
+      </section >
+    </div >
   )
 }
 
